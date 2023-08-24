d80e110ebabc4930d4b43f94599d9f93472c0f3a
Karan Soni <Karanssoni2002@gmail.com>
2023-08-24 11:47:59 +0530

    projects updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://krn1904.github.io/Portfolio',
-  title: 'Hey..',
+  title: 'Hey!',
 }
 
 const about = {
@@ -23,38 +23,45 @@ const projects = [
   {
     name: 'Orbitium',
     description:
-      'Orbitium is a Blockchain based crypto earning Application. You can earn from the Day one of registeration. There are multiple',
+      'Orbitium is a Blockchain based crypto earning Application. You can earn from the Day one of registration. There are multiple',
     stack: ['Postgres', 'TypeScript', 'React', 'NodeJs', 'TypeORM', 'Blockchain'],
   },
   {
     name: 'Tic-Tac-toe',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: [ 'TypeScript', 'React'],
+      "Built in Java using Android Studio, my project is a classic Tic-Tac-Toe game. It's a two-player game designed for fun and entertainment.",
+    stack: [ 'Java', 'Android-Studio'],
     sourceCode: 'https://github.com/krn1904/Tic-Tac-Toe',
     // livePreview: 'https://github.com',
   },
   {
     name: 'Book-E-Sell',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: [ 'TypeScript', 'React'],
+      "Introducing 'Book-e-Sell,' a library management project that brings all available books to one convenient platform.",
+    stack: [ 'React','Nodejs','mongoDb'],
     sourceCode: 'https://github.com/krn1904/Book-management',
     // livePreview: 'https://github.com',
   },
   {
     name: 'Currency Converter',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Explore the Currency Converter project, a tool that simplifies currency conversion for a specific set of currencies',
+    stack: ['Android-Studio', 'Java'],
     sourceCode: 'https://github.com/krn1904/CurrencyConverter', 
     // livePreview: 'https://github.com',
   },
   {
-    name: 'TextUtils - It is a text analyzer, word counter and converter',
+    name: 'TextUtils',
     description:
-      '',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'TextUtils: Your ultimate text companion. A versatile tool offering text analysis, word counting, and conversion capabilities."',
+    stack: ['JavaScript', 'HTML', 'CSS'],
+    sourceCode: "https://github.com/krn1904/textutils",
+  },
+  {
+    name: 'Chat-Application',
+    description:
+      'Currently working on the chat-application. where people can come and chat with the friends.This will be implemented with"',
+    stack: ['TypeScript', 'React'],
     sourceCode: "https://github.com/krn1904/textutils",
   },
  
@@ -78,6 +85,10 @@ const skills = [
 const contact = {
   // email is optional - if left empty Contact section won't show up
   email: 'Karanssoni2002@gmail.com',
+}
+
+const workExperience = {
+
 }
 
 export { header, about, projects, skills, contact }
